cb6ec03fe7a3b4ad73dce23f55b68a876fce673d
Yuichiro <ikariyuichiro@gmail.com>
2026-05-05 23:25:36 +0200

    Fix: python code with flask

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Azure AKS with ArgoCD!"}
+@app.route('/')
+def hello():
+    return "Hello, AKS is working!"
+
+if __name__ == "__main__":
+    
+    app.run(host='0.0.0.0', port=8000)
